e55d066b76c7e0cb72171ac67748c3584c64a135
U0450013 <35754414+U0450013@users.noreply.github.com>
2021-09-24 12:50:12 -0600

    reading files, poor way of setting attributes

--- DecisionTree/decisiontree.py
+++ DecisionTree/decisiontree.py
@@ -1,11 +1,38 @@
-import tree
 import math
+import pandas as pd
+
+
+class Node:
+    def __init__(self, _attribute):
+        self.attribute = _attribute
+        self.branches = []
 
 
 class DecisionTree:
-    def __init__(self, test_data, gain_type, max_depth):
-        self.data = test_data
+    def __init__(self, train_data, gain_type, max_depth):
+        self.data = train_data
         self.max_depth = max_depth
+        self.root = self.buildtree(self.data)
 
-    def info_gain(self):
+    def buildtree(self, current_Set):
+        # if current set has the same label set this as a leaf with the aforementioned label
+        current_Set
+
+        # Find the best attribute
+
+        # Set that as the attribute for the root
+        root = Node(best_attribute)
+        # Split the branches into subsets for each attribute label
+        # If a label is empty set it as the most common label inside of this attribute
+        # Build a tree for every branch
+
+        return root
+
+    def IG(self, set_A, attribute):
+        return 1
+
+    def ME(self, set_A, attribute):
+        return 1
+
+    def GI(self, set_A, attribute):
         return 1
