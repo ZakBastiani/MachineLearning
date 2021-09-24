4cda857502735c977dbdfd81e834a23d2c35a7ce
U0450013 <35754414+U0450013@users.noreply.github.com>
2021-09-24 11:40:14 -0600

    reading files, poor way of setting attributes

--- DecisionTree/main.py
+++ DecisionTree/main.py
@@ -2,13 +2,12 @@ import numpy
 import decisiontree
 import pandas as pd
 
-CSVfile = "Car_Data/train.csv"
+attribute_file = "data-desc.tex"
+train_data_csv = "Car_Data/train.csv"
+test_data_csv = "Car_Data/test.csv"
 
-with open(CSVfile, 'r') as f:
-    for line in f:
-        terms = line.strip().split(',')
-        print(terms)
+attributes = ['buying', 'maint', 'doors', 'persons', 'lug_boot', 'safety', 'label']
+train_df = pd.read_csv(train_data_csv, names=attributes)
+test_df = pd.read_csv(test_data_csv, names=attributes)
 
-train_df = pd.read_csv(CSVfile)
 
-print(train_df)
