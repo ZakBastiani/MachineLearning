c0ff2350932f44d5bec7e646ba588781416c4f4f
U0450013 <35754414+U0450013@users.noreply.github.com>
2021-11-02 13:25:57 -0600

    Added print statements and some csv saves

--- Perceptron/test.py
+++ Perceptron/test.py
@@ -18,20 +18,34 @@ test_df = pd.read_csv(test_data_csv, names=attributes).astype(float)
 train_df.loc[train_df['label'] == 0, 'label'] = -1
 test_df.loc[test_df['label'] == 0, 'label'] = -1
 
-lr = 0.00001
+lr = 0.000001
 
 stand = standard_perceptron.StandPerceptron(train_df, 10, lr)
 pred = stand.predict(test_df.drop(columns=['label']))
 
 print('Standard Acc: ' + str(sum(abs(pred['pred'] - test_df['label']))/2/len(test_df)))
+print(stand.w)
 
 voted = voted_perceptron.VotedPerceptron(train_df, 10, lr)
 pred = voted.predict(test_df.drop(columns=['label']))
 
 print('Voted Acc: ' + str(sum(abs(pred['pred'] - test_df['label']))/2/len(test_df)))
 
+np.savetxt('w_list.csv', voted.w_list, delimiter=',')
+np.savetxt('c_list.csv', voted.c_list, delimiter=',')
+form = "{:.4f}"
+for i in range(len(voted.w_list)):
+    output = form.format(voted.c_list[i]) + ' & '
+    for j in range(len(voted.w_list[i])-1):
+        output += form.format(voted.w_list[i][j]) + ' & '
+    output += form.format(voted.w_list[i][len(voted.w_list[i])-1]) + '\\\\ \\hline'
+    print(output)
+
+
 avg = average_perceptron.AvgPerceptron(train_df, 10, lr)
 pred = avg.predict(test_df.drop(columns=['label']))
 
 print('Average Acc: ' + str(sum(abs(pred['pred'] - test_df['label']))/2/len(test_df)))
+print(avg.a)
+
 
